6d9c74e3f2d7820cae9330d6142e3f7991971867
prityushchandra <43948945+prityushchandra@users.noreply.github.com>
2019-10-20 16:47:06 +0530

    color_detection
    red,green,blue clor detection using python and opencv

--- color_detect.py
+++ color_detect.py
@@ -2,19 +2,69 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+
+def nothing(x):
+	pass
+
+def get_frame(cap, scaling_factor):
+    # Capture the frame from video capture object
+    ret, frame = cap.read()
+
+    # Resize the input frame
+    frame = cv2.resize(frame, None, fx=scaling_factor,
+            fy=scaling_factor, interpolation=cv2.INTER_AREA)
+
+    return frame
+
+if __name__=='__main__':
+    cap = cv2.VideoCapture(0)
+    scaling_factor = 0.5
+
 
 def greenCircleDetect():
+	#ret, frame = cap.read()
+	#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	lower_blue = np.array([36, 0, 0])
+	upper_blue = np.array([86, 255, 255])
+	mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
-
-def redCircleDetect():
+	contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+	cv2.drawContours(frame, contours, -1, (255, 255, 0))
+	#cv2.imshow('frame', frame)
+	#cv2.imshow('green_output', mask)
 
 
 def blueCircleDetect():
+	#ret, frame = cap.read()
+	#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	lower_blue = np.array([62, 146, 51])
+	upper_blue = np.array([179, 255, 100])
+	mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
+	contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+	cv2.drawContours(frame, contours, -1, (0, 255, 0))
+	#cv2.imshow('frame', frame)
+	#cv2.imshow('blue_output', mask)
+
+
+
+def redCircleDetect():
+	#ret, frame = cap.read()
+	#hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	lower_blue = np.array([0, 166, 52])
+	upper_blue = np.array([179, 255, 255])
+	mask = cv2.inRange(hsv, lower_blue, upper_blue)
+
+	contours, _ = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+	cv2.drawContours(frame, contours, -1, (0, 255, 0))
+
+	#cv2.imshow('frame', frame)
+	#cv2.imshow('red_output', mask)
 
 while True:
-	ret, frame = cap.read()
+
+	frame = get_frame(cap, scaling_factor)##scaling factor is defined as size of window you want
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
 	greenCircleDetect()
 
@@ -22,10 +72,15 @@ while True:
 
 	blueCircleDetect()
 
-
 	cv2.imshow('frame', frame)
 
-	cv2.waitKey(3)
+
+
+	key=cv2.waitKey(1)
+	if key==27:
+		break
+
+
 
 cap.release()
 cv2.destroyAllWindows()
